9eb848b2afa6f5be4a4f225365eb8af4931789bb
Patrick Granahan <pjgranahan@gmail.com>
2015-11-15 13:55:52 -0600

    Added examples to Quanto LT

--- Quanto.py
+++ Quanto.py
@@ -40,49 +40,64 @@ class Quanto(OptionValuation):
         Examples
         -------
 
+        Calculate the price of a Quanto option. This example comes from Hull ch.30, ex.30.5 (p.701-702)
+
         >>> s = Stock(S0=1200, vol=.25, q=0.015)
-        >>> o = Quanto(ref=s, right='call', K=1200, T=2, rf_r=.03, frf_r=0.05, desc='179.83, Hull p.701-702 ex.30.5')
-
+        >>> o = Quanto(ref=s, right='call', K=1200, T=2, rf_r=.03, frf_r=0.05)
         >>> o.calc_px(method='LT', nsteps=100, vol_ex=0.12, correlation=0.2, keep_hist=True).px_spec.px
-        179.83
+        179.82607364328157
 
-        >>> o.px_spec.ref_tree
-        ((50.000000000000014,),
-         (37.0409110340859, 67.49294037880017),
-         (27.440581804701324, 50.00000000000001, 91.10594001952546))
+        >>> o.px_spec.ref_tree # doctest: +ELLIPSIS
+        ((1199.999999999993,), (1158.3148318698472, 1243.1853243866492), ... 38364.96926881886, 41175.99589789209))
 
         >>> o.calc_px(method='LT', nsteps=100, keep_hist=False)
-        American
-        K: 52
+        Quanto.Quanto
+        K: 1200
         T: 2
-        _right: put
-        _signCP: -1
-        desc: 7.42840, Hull p.288
-        frf_r: 0
+        _right: call
+        _signCP: 1
+        frf_r: 0.05
         px_spec: qfrm.PriceSpec
           LT_specs:
-            a: 1.0512710963760241
-            d: 0.7408182206817179
+            a: 1.0003000450045003
+            d: 0.965262359891545
             df_T: 0.9048374180359595
-            df_dt: 0.951229424500714
-            dt: 1.0
-            p: 0.5097408651817704
-            u: 1.3498588075760032
+            df_dt: 0.999000499833375
+            dt: 0.02
+            p: 0.49540447909174495
+            u: 1.0359877703222138
           keep_hist: false
           method: LT
-          nsteps: 2
-          px: 7.42840190270483
+          nsteps: 100
+          px: 172.20505562521683
           sub_method: binomial tree; Hull Ch.13
         ref: qfrm.Stock
-          S0: 50
+          S0: 1200
           curr: null
           desc: null
-          q: 0
+          q: 0.015
           tkr: null
-          vol: 0.3
-        rf_r: 0.05
+          vol: 0.25
+        rf_r: 0.03
         seed0: null
+        <BLANKLINE>
 
+        Calculate the price of a Quanto option. This example comes from Hull ch.30, problem.30.9.b (p.704)
+
+        >>> s = Stock(S0=400, vol=.2, q=0.03)
+        >>> o = Quanto(ref=s, right='call', K=400, T=2, rf_r=.06, frf_r=0.04)
+        >>> o.calc_px(method='LT', nsteps=100, vol_ex=0.06, correlation=0.4).px_spec.px
+        57.50700503047851
+
+        Example of option price development (LT method) with increasing maturities
+
+        >>> from pandas import Series
+        >>> expiries = range(1,11)
+        >>> O = Series([o.update(T=t).calc_px(method='LT', nsteps=100, vol_ex=0.12, correlation=0.2).px_spec.px for t in expiries], expiries)
+        >>> O.plot(grid=1, title='Price vs expiry (in years)') # doctest: +ELLIPSIS
+        <matplotlib.axes._subplots.AxesSubplot object at ...>
+        >>> import matplotlib.pyplot as plt
+        >>> plt.show()
         """
         self.px_spec = PriceSpec(method=method, nsteps=nsteps, npaths=npaths, keep_hist=keep_hist,
                                  vol_ex=vol_ex, correlation=correlation)
@@ -99,63 +114,28 @@ class Quanto(OptionValuation):
 
         """
 
-        # Explicit imports
-
-        # Define parameters
-        T = self.T  # Time to expiry (years)
-        S0 = self.ref.S0  # Exchange rate (domestic/foreign)
-        K = self.K  # Delivery Price (domestic)
-        rf_r = self.rf_r  # Interest rate for Domestic Currency (% / year)
-        frf_r = self.frf_r  # Interest rate for Foreign Currency (% / year)
-        vol = self.ref.vol  # Volatility of Underlying (% / year)
+        # Get provided parameters
         vol_ex = getattr(self.px_spec, 'vol_ex')  # Volatility of the exchange rate
-        q = self.ref.q  # Dividend Yield of Underlying
-        correlation = getattr(self.px_spec, 'correlation')  # Correlation of Asset to Domestic Currency
-
-        # Get additional provided parameters
+        correlation = getattr(self.px_spec, 'correlation')  # Correlation between asset and exchange rate
         keep_hist = getattr(self.px_spec, 'keep_hist', False)
         n = getattr(self.px_spec, 'nsteps', 3)
-        _ = self.LT_specs(n)
 
         # Compute the foreign numeraire dividend yield
-        growth_rate_of_underlying = (correlation * vol * vol_ex)
-        domestic_numeraire = rf_r - q
+        growth_rate_of_underlying = (correlation * self.ref.vol * vol_ex)
+        domestic_numeraire = self.rf_r - self.ref.q
         foreign_numeraire = domestic_numeraire + growth_rate_of_underlying
-        foreign_numeraire_dividend_yield = frf_r - foreign_numeraire
+        foreign_numeraire_dividend_yield = self.frf_r - foreign_numeraire
 
-        stock = Stock(S0=S0, vol=vol, q=foreign_numeraire_dividend_yield)
+        # Once we have the foreign numeraire dividend yield calculated,
+        # we can price the Quanto option using an American option with specific parameters
         from American import American
-        american_option = American(ref=stock, right=self.right, K=K, T=T, rf_r=frf_r)
+        stock = Stock(S0=self.ref.S0, vol=self.ref.vol, q=foreign_numeraire_dividend_yield)
+        american_option = American(ref=stock, right=self.right, K=self.K, T=self.T, rf_r=self.frf_r)
 
+        # Then we take the price spec from the American option
         self.px_spec = american_option.calc_px(method='LT', nsteps=n, keep_hist=keep_hist).px_spec
 
         return self
-        # # Compute final nodes
-        # S = S0 * _['d'] ** arange(n, -1, -1) * _['u'] ** arange(0, n + 1)  # terminal stock prices
-        # O = maximum(self.signCP * (S - K), 0)  # terminal option payouts
-        #
-        # # Initialize tree structure
-        # S_tree = (tuple([float(s) for s in S]),)  # use tuples of floats (instead of numpy.float)
-        # O_tree = (tuple([float(o) for o in O]),)
-        #
-        #
-        # for i in range(n, 0, -1):
-        #     O = _['df_dt'] * ((1 - _['p']) * O[:i] + (_['p']) * O[1:])  # prior option prices (@time step=i-1)
-        #     S = _['d'] * S[1:i + 1]  # prior stock prices (@time step=i-1)
-        #     Payout = maximum(self.signCP * (S - K), 0)  # payout at time step i-1 (moving backward in time)
-        #     O = maximum(O, Payout)
-        #
-        #     # Build tree
-        #     S_tree = (tuple([float(s) for s in S]),) + S_tree
-        #     O_tree = (tuple([float(o) for o in O]),) + O_tree
-        #
-        #
-        # # Record price specs
-        # self.px_spec.add(px=float(Util.demote(O)), method='LT', sub_method='binomial tree; Hull Ch.13', LT_specs=_)
-        # if keep_hist:
-        #     self.px_spec.add(ref_tree=S_tree, opt_tree=O_tree)
-        #
-        # return self
 
     def _calc_BS(self):
         """ Internal function for option valuation.
