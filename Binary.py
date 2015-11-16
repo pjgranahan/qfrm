0176da53e7aeb713bd474561b336b25a8d7ab12d
TY YAO <ty13@rice.edu>
2015-11-15 22:09:54 -0600

    added more examples for the LT method

--- Binary.py
+++ Binary.py
@@ -120,6 +120,8 @@ class Binary(OptionValuation):
         <matplotlib.axes._subplots.AxesSubplot object at ...>
         >>> import matplotlib.pyplot as plt
         >>> plt.show()
+
+
         """
         self.px_spec = PriceSpec(method=method, sub_method=payout_type, nsteps=nsteps, npaths=npaths,
                                  keep_hist=keep_hist, payout_type=payout_type, Q=Q)
@@ -193,6 +195,67 @@ class Binary(OptionValuation):
 
         .. note::
         Implementing Binomial Trees:   http://papers.ssrn.com/sol3/papers.cfm?abstract_id=1341181
+
+        Examples
+        -------
+        #Example of using _calc_LT method
+        >>> s = Stock(S0=42, vol=.20)
+        >>> o = Binary(ref=s, right='put', K=40, T=.5, rf_r=.1)
+        >>> print(o.calc_px(method='LT', nsteps=40,payout_type="asset_or_nothing").px_spec)
+        qfrm.PriceSpec
+        LT_specs:
+          a: 1.0012507815756226
+          d: 0.9778874672052759
+          df_T: 0.951229424500714
+          df_dt: 0.9987507809245809
+          dt: 0.0125
+          p: 0.5223760586914103
+          u: 1.0226125536284048
+        Q: 0.0
+        keep_hist: false
+        method: LT
+        nsteps: 40
+        payout_type: asset_or_nothing
+        px: 8.952897200837354
+        sub_method: asset_or_nothing
+        <BLANKLINE>
+        >>> print(o.update(right='put').calc_px().px_spec.px)
+        9.276485780407903
+
+        >>> s = Stock(S0=50, vol=.3)
+        >>> o = Binary(ref=s, right='call', K=40, T=2, rf_r=.05)
+        >>> print(o.calc_px(method='LT', nsteps=60, payout_type="cash_or_nothing", Q=1000).px_spec)
+        qfrm.PriceSpec
+        LT_specs:
+          a: 1.001668056327482
+          d: 0.9467007290508975
+          df_T: 0.9048374180359595
+          df_dt: 0.9983347214509387
+          dt: 0.03333333333333333
+          p: 0.5015299486608089
+          u: 1.0563000210241065
+        Q: 1000
+        keep_hist: false
+        method: LT
+        nsteps: 60
+        payout_type: cash_or_nothing
+        px: 904.8374180359497
+        sub_method: cash_or_nothing
+        <BLANKLINE>
+
+        >>> print(o.update(right='put').calc_px().px_spec.px)
+        8.2540367580782
+
+        Example of option price development (LT method) with increasing maturities
+
+        >>> from pandas import Series
+        >>> expiries = range(1,11)
+        >>> O = Series([o.update(T=t).calc_px(method='LT', payout_type="cash_or_nothing", Q=1000).px_spec.px for t in expiries], expiries)
+        >>> O.plot(grid=1, title='Price vs expiry (in years)') # doctest: +ELLIPSIS
+        <matplotlib.axes._subplots.AxesSubplot object at ...>
+        >>> import matplotlib.pyplot as plt
+        >>> plt.show()
+
 
         """
         from numpy import cumsum, log, arange, insert, exp, sqrt, sum, maximum
@@ -277,7 +340,3 @@ class Binary(OptionValuation):
         """
         return self
 
-s = Stock(S0=42, vol=.20)
-o = Binary(ref=s, right='put', K=40, T=.5, rf_r=.1)
-print(o.calc_px(method='LT', payout_type="asset_or_nothing").px_spec)
-print(o.px_spec.opt_tree)
