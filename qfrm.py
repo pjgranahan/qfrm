7f0819b33023bae85a2c66290612145d344427da
omelnikov <xisreal@gmail.com>
2015-11-25 15:53:17 -0600

    deleted old code

--- qfrm.py
+++ qfrm.py
@@ -18,8 +18,3 @@ from Quanto import *
 from Shout import *
 from VarianceSwap import *
 
-
-# s = Stock(S0=30, vol=.3)
-# o = European(ref=s, right='call', K=30, T=1., rf_r=.08, desc='Example from Internet')
-# o.calc_px(method='BS')
-# print(o.px_spec)
