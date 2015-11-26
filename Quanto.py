9305bbcd3f5c4ead3ff7927973ef35bba88c2eb0
Runmin Zhang <z.runmin@gmail.com>
2015-11-26 13:14:53 -0600

    Quanto: Add MC method.

--- Quanto.py
+++ Quanto.py
@@ -8,7 +8,7 @@ class Quanto(OptionValuation):
     Inherits all methods and properties of OptionValuation class.
     """
 
-    def calc_px(self, method='BS', nsteps=None, npaths=None, keep_hist=False, vol_ex=0.0, correlation=0.0):
+    def calc_px(self, method='BS', nsteps=None, npaths=None, keep_hist=False, vol_ex=0.0, correlation=0.0, seed=1,deg=5):
         """ Wrapper function that calls appropriate valuation method.
 
         User passes parameters to calc_px, which saves them to local PriceSpec object
@@ -101,7 +101,7 @@ class Quanto(OptionValuation):
         >>> plt.show()
         """
         self.px_spec = PriceSpec(method=method, nsteps=nsteps, npaths=npaths, keep_hist=keep_hist,
-                                 vol_ex=vol_ex, correlation=correlation)
+                                 vol_ex=vol_ex, correlation=correlation,seed=1,deg=deg)
         return getattr(self, '_calc_' + method.upper())()
 
     def _calc_LT(self):
@@ -149,7 +149,13 @@ class Quanto(OptionValuation):
         Note
         ----
 
+
         """
+
+
+
+
+
         return self
 
     def _calc_MC(self):
@@ -163,8 +169,51 @@ class Quanto(OptionValuation):
 
         Note
         ----
-
+        [1] http://unicreditanduniversities.eu/uploads/assets/QuantoAdjustments_in_the_Presence_of_SV_Giese.pdf
         """
+
+        from math import sqrt
+        from numpy import cumsum, maximum, sum, where, polyval, polyfit, exp, random, mean, copy
+        from numpy.random import normal, seed
+        from pandas import DataFrame
+        import matplotlib.pyplot as plt
+
+        try: method = self.px_spec.sub_method
+        except TypeError: pass
+        try: deg = self.px_spec.deg
+        except TypeError: deg = 5
+
+        vol_ex = getattr(self.px_spec, 'vol_ex')  # Volatility of the exchange rate
+        correlation = getattr(self.px_spec, 'correlation')  # Correlation between asset and exchange rate
+        n_steps = getattr(self.px_spec, 'nsteps', 3)
+        n_paths = getattr(self.px_spec, 'npaths', 3)
+        _ = self
+
+
+        # Compute the foreign numeraire dividend yield
+        growth_rate_of_underlying = (correlation * self.ref.vol * vol_ex)
+        domestic_numeraire = self.rf_r - self.ref.q
+        foreign_numeraire = domestic_numeraire + growth_rate_of_underlying
+        foreign_numeraire_dividend_yield = self.frf_r - foreign_numeraire
+
+        # Once we have the foreign numeraire dividend yield calculated,
+        # we can price the Quanto option using an American option with specific parameters. Compare the
+
+        dt = _.T / n_steps; df = exp(-_.frf_r * dt)
+        signCP = 1 if _.right.lower()[0] == 'c' else -1
+
+        seed(_.px_spec.seed)
+        S = _.ref.S0 * exp(cumsum(normal((_.frf_r-foreign_numeraire_dividend_yield- 0.5 * _.ref.vol ** 2) * dt, _.ref.vol * sqrt(dt), (n_steps + 1, n_paths)), axis=0)); S[0] = _.ref.S0
+        payout = maximum(signCP * (S - _.K), 0); v = copy(payout)  # terminal payouts
+
+        for i in range(n_steps - 1, 0, -1):    # American Option Valuation by Backwards Induction
+            rg = polyfit(S[i], v[i + 1] * df, deg)      # fit 5th degree polynomial to PV of current inner values
+            C = polyval(rg, S[i])              # continuation values.
+            v[i] = where(payout[i] > C, payout[i], v[i + 1] * df)  # exercise decision
+        v[0] = v[1] * df
+        print(mean(v[0]))
+        self.px_spec.add(px=float(mean(v[0])))
+
         return self
 
     def _calc_FD(self):
@@ -182,3 +231,9 @@ class Quanto(OptionValuation):
         """
 
         return self
+
+s = Stock(S0=1200, vol=.25, q=0.015)
+o = Quanto(ref=s, right='call', K=1200, T=2, rf_r=.03, frf_r=0.05)
+
+print(o.calc_px(method='LT', nsteps=100, vol_ex=0.12, correlation=0.2).px_spec.px)
+o.calc_px(method='MC',nsteps=100,npaths=5000, vol_ex=0.12, correlation=0.2, deg=5).px_spec
