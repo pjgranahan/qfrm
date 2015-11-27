014da041c7adeccb56e6bcd0351560f340e790c7
Scott Morgan <morgan.scott.p@gmail.com>
2015-11-27 15:26:37 -0500

    - Beginning of Compound Option FD method

--- Compound.py
+++ Compound.py
@@ -1,13 +1,16 @@
 from OptionValuation import *
+from European import European
+from American import American
+import numpy as np
 
-class Spread(OptionValuation):
+class Compound(OptionValuation):
     """ Asian option class.
 
     Inherits all methods and properties of OptionValuation class.
     """
 
 
-    def calc_px(self, method='BS', on = 'put', nsteps=None, npaths=None, keep_hist=False):
+    def calc_px(self, option, method='BS', on = 'put', nsteps=None, npaths=None, keep_hist=False):
         """ Wrapper function that calls appropriate valuation method.
 
         User passes parameters to calc_px, which saves them to local PriceSpec object
@@ -50,6 +53,7 @@ class Spread(OptionValuation):
 
         self.px_spec = PriceSpec(method=method, nsteps=nsteps, npaths=npaths, keep_hist=keep_hist)
         self.on = on
+        self.o = option
         return getattr(self, '_calc_' + method.upper())()
 
     def _calc_LT(self):
@@ -121,18 +125,61 @@ class Spread(OptionValuation):
         ----
 
         """
-        from numpy import arange, zeros
-        from pandas import DataFrame
 
         _ = self.px_spec
 
-        npaths = getattr(_, 'npaths', 3)
-        nsteps = getattr(_, 'nsteps', 3)
-        grid = DataFrame(zeros([npaths,nsteps]))
-        
+        M = getattr(_, 'npaths', 3)
+        N = getattr(_, 'nsteps', 3)
 
+        signCP = 1 if self.right.lower() == 'call' else -1
+        x = np.matrix(np.zeros(shape=(N+1,M+1)))
 
+        T = self.T
+        T_left = self.o.T - self.T
+        orig_T = self.o.T
+        vol = self.o.ref.vol
+        Smax = 2*self.o.ref.S0
+        r = self.o.rf_r
+        q = 0
+        dt = T/(N+0.0)
+        dS = Smax/(M+0.0)
+        N = int(T/dt)
+        M = int(Smax/dS)
+        K = self.K
+        S0 = self.o.ref.S0
 
+        def a(j):
+            return .5*dt*((vol**2)*(j**2)-(r-q)*j)
+        def b(j):
+            return 1 - dt*((vol**2)*(j**2)+r)
+        def c(j):
+            return .5*dt*((vol**2)*(j**2)+(r-q)*j)
 
+        temp = self.o
+        self.o.T = T_left
 
+        # replace with LT Price for each one
+        for i in range(0,M+1):
+            self.o.ref.S0 = dS*i
+            x[N,i] = np.maximum(signCP*(self.o.calc_px(method='LT',nsteps = 20).px_spec.px-K),0)
+            #print(x[N,i])
+
+        #x[N,:] = np.matrix(np.maximum(signCP*(np.linspace(dS*M,0,num=M+1,endpoint=True) - K),0) ).\
+            #transpose().transpose()
+
+        x[:,0] = np.matrix(list(map(lambda i: (np.maximum(signCP*(Smax - K*np.exp(-r*(N-i)*dt)),0)), range(0,N+1)))).transpose()
+
+        for i in np.arange(N-1,-1,-1):
+            for k in range(1,M):
+                j = M-k
+                x[i,k] = a(j)*x[i+1,k+1] + b(j)*x[i+1,k] + c(j)*x[i+1,k-1]
+
+        self.o.ref.S0 = S0
+        self.o.T = orig_T
+        print(x[0,M-self.o.ref.S0/dS])
         return self
+
+s = Stock(S0=90, vol=.12, q=.04)
+o = European(ref=s, right='call', K=80, T=.5, rf_r=.05, desc='7.42840, Hull p.288')
+o2 = Compound(right='call',T=.25, K = 20)
+o2.calc_px(method='FD',option=o,npaths=100,nsteps = 10000)
